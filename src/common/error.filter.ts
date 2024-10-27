1c86db6f47739ba276a1fd6953174cd44563cbe0
maulanadityaa <maulanadityaaa@gmail.com>
2024-10-27 15:31:59 +0700

    [UPDATE] setup project

---
+++ src/common/error.filter.ts
@@ -0,0 +1,31 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { ZodError } from 'zod';
+
+@Catch(ZodError, HttpException)
+export class ErrorFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const response = host.switchToHttp().getResponse();
+
+    if (exception instanceof HttpException) {
+      response.status(exception.getStatus()).json({
+        statusCode: exception.getStatus(),
+        errors: exception.getResponse(),
+      })
+    } else if (exception instanceof ZodError) {
+      response.status(400).json({
+        statusCode: 400,
+        errors: exception.errors,
+      });
+    } else {
+      response.status(500).json({
+        statusCode: 500,
+        errors: exception.message,
+      });
+    }
+  }
+}
